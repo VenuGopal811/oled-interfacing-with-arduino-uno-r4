127f971c7175a5d263bcbf08d980aea31d61d45d
Alpha_VG <imvenugopal08@gmail.com>
2026-02-10 09:38:08 +0530

    initialize OLED display and update setup function

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_ADDR 0x3C
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT);
+
 void setup() {
-// write your initialization code here
+    Serial.begin(9600);
+    if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR)) {
+        Serial.println(F("OLED not found"));
+        while (true);
+    }
+    //Clear Display
+    display.clearDisplay();
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(0, 0);
+    display.println("Hello, my name is slim shady");
+    display.display();
 }
 
 void loop() {
